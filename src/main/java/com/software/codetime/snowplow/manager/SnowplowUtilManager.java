59f5e2176a83c9da545f013a39305d6cc6ffd5fc
xavluiz <35306917+xavluiz@users.noreply.github.com>
2023-09-25 08:53:34 -0700

    fix no class def found exceptions

--- src/main/java/com/software/codetime/snowplow/manager/SnowplowUtilManager.java
+++ src/main/java/com/software/codetime/snowplow/manager/SnowplowUtilManager.java
@@ -2,20 +2,13 @@ package com.software.codetime.snowplow.manager;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import com.software.codetime.main.PluginInfo;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Logger;
 
 public class SnowplowUtilManager {
     public static final Gson gson = new GsonBuilder().create();
-
-    private static final Logger LOG = Logger.getLogger("UtilManager");
 
     public static String currentDay = null;
 
@@ -27,33 +20,10 @@ public class SnowplowUtilManager {
     }
 
     public static JavaTrackerInfo getTrackerInfo() {
-        Model mavenModel = getMavenInfo();
         trackerInfo = new JavaTrackerInfo();
-        trackerInfo.artifactId = mavenModel.getArtifactId();
-        trackerInfo.version = mavenModel.getVersion();
+        trackerInfo.artifactId = "codetime-intellij";
+        trackerInfo.version = PluginInfo.getVersion();
         return trackerInfo;
-    }
-
-    private static Model getMavenInfo() {
-        MavenXpp3Reader reader = new MavenXpp3Reader();
-        Model model;
-        try {
-            if ((new File("pom.xml")).exists()) {
-                model = reader.read(new FileReader("pom.xml"));
-            } else {
-                model = reader.read(
-                        new InputStreamReader(
-                                SnowplowUtilManager.class.getResourceAsStream("pom.xml")
-                        )
-                );
-            }
-        } catch (Exception e) {
-            model = new Model();
-            model.setArtifactId("swdc-java-ops");
-            model.setGroupId("swdc.java.ops");
-            model.setVersion("1.1.4");
-        }
-        return model;
     }
 
     public static boolean isNewDay() {
