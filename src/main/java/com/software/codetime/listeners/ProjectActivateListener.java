ee6133ebee4f8cb54a105d2f1c4de6aaaa832261
xavluiz <35306917+xavluiz@users.noreply.github.com>
2024-10-26 14:03:13 -0700

    bump until version and fix deprecated startup

--- src/main/java/com/software/codetime/listeners/ProjectActivateListener.java
+++ src/main/java/com/software/codetime/listeners/ProjectActivateListener.java
@@ -1,21 +1,34 @@
 package com.software.codetime.listeners;
 
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.EditorFactory;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManagerListener;
+import com.intellij.openapi.startup.ProjectActivity;
 import com.software.codetime.main.Activator;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class ProjectActivateListener implements ProjectManagerListener {
+public class ProjectActivateListener implements ProjectActivity {
     private static Project currentProject;
 
+    @Nullable
     @Override
-    public void projectOpened(@NotNull Project project) {
-        currentProject = project;
-        ProjectManagerListener.super.projectOpened(project);
+    public Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
         Activator.getInstance();
+        currentProject = project;
+        return currentProject;
     }
 
     public static Project getCurrentProject() {
+        if (currentProject == null) {
+            Editor[] editors = EditorFactory.getInstance().getAllEditors();
+            if (editors != null && editors.length > 0) {
+                currentProject = editors[0].getProject();
+            }
+        }
         return currentProject;
     }
 }
